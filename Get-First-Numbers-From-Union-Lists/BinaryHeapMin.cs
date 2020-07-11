89633bc3647cebb12bfbbcc059b604eb381ee1f1
Ivan <chek539camo@gmail.com>
2020-07-11 13:44:18 +0500

    Initial commit

---
+++ Get-First-Numbers-From-Union-Lists/BinaryHeapMin.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Get_First_Numbers_From_Union_Lists
+{
+    public class BinaryHeapMin
+    {
+        private List<int> list = new List<int>();
+        public int Limit { get; set; }
+
+        private int HeapSize => list.Count;
+
+        public void Add(int value)
+        {
+            list.Add(value);
+            var i = HeapSize - 1;
+            var parent = (i - 1) / 2;
+
+            while (i > 0 && list[parent] > list[i])
+            {
+                var temp = list[i];
+                list[i] = list[parent];
+                list[parent] = temp;
+                i = parent;
+                parent = (i - 1) / 2;
+            }
+        }
+        
+        public void Heapify(int i)
+        {
+            while (true)
+            {
+                var leftChild = 2 * i + 1;
+                var rightChild = 2 * i + 2;
+                var leastChild = i;
+
+                if (leftChild < HeapSize && list[leftChild] < list[leastChild]) 
+                {
+                    leastChild = leftChild;
+                }
+
+                if (rightChild < HeapSize && list[rightChild] < list[leastChild])
+                {
+                    leastChild = rightChild;
+                }
+
+                if (leastChild == i) 
+                {
+                    break;
+                }
+
+                var temp = list[i];
+                list[i] = list[leastChild];
+                list[leastChild] = temp;
+                i = leastChild;
+            }
+        }
+        
+        public void BuildHeap(IEnumerable<int> sourceArray)
+        {
+            list = sourceArray.ToList();
+            for (var i = HeapSize / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+        
+        public int GetMin()
+        {
+            var result = list[0];
+            list[0] = list[HeapSize - 1];
+            list.RemoveAt(HeapSize - 1);
+            Heapify(0);
+            return result;
+        }
+        
+        public void HeapSort(int[] array)
+        {
+            BuildHeap(array);
+            for (var i = array.Length - 1; i >= 0; i--)
+            {
+                array[i] = GetMin();
+                Heapify(0);
+            }
+        }
+    }
+}
---
+++ Get-First-Numbers-From-Union-Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Get_First_Numbers_From_Union_Lists
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const int n = 3;
+            const int k = 3;
+            var list = new List<List<int>>();
+            var list1 = new List<int>{1, 3, 9};
+            var list2 = new List<int>{2, 8, 15};
+            var list3 = new List<int>{5, 6, 222};
+            var tree = new BinaryHeapMin{Limit = n};
+            list.Add(list1);
+            list.Add(list2);
+            list.Add(list3);
+            for (var i = 0; i < k; i++)
+            {
+                for (var j = 0; j < list[i].Count; j++)
+                    tree.Add(list[i][j]);
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                Console.WriteLine(tree.GetMin());
+            }
+        }
+    }
+}
